f3475ec3b1c63dd1e835b8e6e4ee7f7acb565263
Oleksii Kutuzov <8535871+oleksiikutuzov@users.noreply.github.com>
2022-11-03 13:04:54 +0100

    Upload script

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # flipperzero-frame
 A simple script to insert a qFlipper screenshot into a Flipper Zero frame
+
+## Requirements
+* `pillow` library
+
+## Usage
+```
+python3 frame.py filename.png
+```
+
+Will output `framed_filename.png` file.
+
+<img src="images/framed_Screenshot.png" width="500px"> 
---
+++ frame.py
@@ -0,0 +1,22 @@
+from PIL import Image
+import sys
+
+# Open screen background
+img1 = Image.open(r"background.png")
+
+# Open screen frame
+img2 = Image.open(r"frame.png")
+
+# Open screenshot itself
+img3 = Image.open(sys.argv[1])
+
+# Paste frame over screen background
+img1.paste(img2, (0, 0), mask=img2)
+
+# Resize the screenshot
+img3 = img3.resize((707, 367), Image.Resampling.LANCZOS)
+
+# Paste the screenshot over the frame with screen background
+img1.paste(img3, (715, 413))
+
+img1.save('framed_' + sys.argv[1])

ea7837500dfdf3a8d2d25d471a18a127871bf17c
Oleksii Kutuzov <8535871+oleksiikutuzov@users.noreply.github.com>
2023-02-10 23:51:18 +0100

    Update resources and script

--- README.md
+++ README.md
@@ -11,4 +11,4 @@ python3 frame.py filename.png
 
 Will output `framed_filename.png` file.
 
-<img src="images/framed_Screenshot.png" width="500px"> 
+<img src="images/framed_Screenshot.png" width="500px">
--- frame.py
+++ frame.py
@@ -14,9 +14,9 @@ img3 = Image.open(sys.argv[1])
 img1.paste(img2, (0, 0), mask=img2)
 
 # Resize the screenshot
-img3 = img3.resize((707, 367), Image.Resampling.LANCZOS)
+img3 = img3.resize((784, 416), Image.Resampling.LANCZOS)
 
 # Paste the screenshot over the frame with screen background
-img1.paste(img3, (715, 413))
+img1.paste(img3, (683, 294))
 
 img1.save('framed_' + sys.argv[1])
